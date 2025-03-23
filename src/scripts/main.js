e532d080e0400ccb0538de5672300b347fd1ad09
lonely43 <iladenisov327@gmail.com>
2025-03-24 01:27:22 +0300

    feat: создание функции

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,60 @@
-const ctx = document.getElementById('myChart');
+var xValues = []
+var yValues = []
 
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
+genChart(0, 2000, 50)
+
+new Chart("myChart", {
+	type: "line",
+	data: {
+		labels: xValues,
+		datasets: [
+			{
+				fill: false,
+				pointRadius: 4,
+				borderColor: "rgba(0, 0, 255, 0.5)",
+				data: yValues
+			}
+		]
+	},
+	options: {
+		plugins: {
+			title: {
+				display: true,
+				text: "Распределение Максвелла-Больцмана",
+				fontSize: 16
+			}
+		},
+		scales: {
+			x: {
+				title: {
+					display: true,
+					text: 'V (м/с)' // Подпись для оси X
+				}
+			},
+			y: {
+				title: {
+					display: true,
+					text: 'F(x) (с / м)' // Подпись для оси Y
+				},
+            reverse: false
+			}
+		}
+	}
 });
+
+function maksvelsChart(v) {
+	const M = 0.032 // молекулярная масса воздуха в кг/моль
+	const R = 8.314 // универсальная газовая постоянная в Дж/(моль·К)
+	const T = 300 // температура в К
+
+	const coefficient = Math.pow(M / (2 * Math.PI * R * T), 3 / 2)
+	const exponential = Math.exp(-((M * Math.pow(v, 2)) / (2 * R * T)))
+	return 4*Math.PI * coefficient * Math.pow(v, 2) * Math.E * exponential
+}
+
+function genChart(i1, i2, step = 1) {
+	for (let x = i1; x <= i2; x += step) {
+		yValues.push(maksvelsChart(x))
+		xValues.push(x)
+	}
+}
