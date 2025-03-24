f997ddd716bed7a084aa296f0dde1e69c602f198
lonely43 <iladenisov327@gmail.com>
2025-03-25 01:29:23 +0300

    feat: make structure for scripts & create multi charts

---
+++ src/scripts/Chart.js
@@ -0,0 +1,89 @@
+export default class ChartSystem {
+	constructor() {
+		this.datasetsOptions = []
+		this.MyChart = ""
+	}
+
+	createCharts(xValues) {
+		if (this.MyChart !== "") {
+			this.MyChart.destroy()
+		}
+
+		console.log(this.datasetsOptions)
+      
+		this.MyChart = new Chart("myChart", {
+			type: "line",
+			data: {
+				labels: xValues,
+				datasets: this.datasetsOptions
+			},
+			options: {
+				plugins: {
+					title: {
+						display: true,
+						text: "Распределение Максвелла-Больцмана"
+					},
+					legend: {
+						display: false
+					}
+				},
+				scales: {
+					x: {
+						title: {
+							display: true,
+							text: "V (м/с)", // Подпись для оси X
+							align: "end",
+							color: "rgb(0, 0, 0)"
+						}
+					},
+					y: {
+						title: {
+							display: true,
+							text: "F(x) (м/c)", // Подпись для оси Y
+							align: "end",
+							color: "rgb(0, 0, 0)"
+						}
+					}
+				}
+			}
+		})
+	}
+
+	addChart(vmax, M, T, id) {
+		let xValues = []
+		let yValues = []
+      const step = 50
+
+      for (let x = 0; x <= vmax; x += step) {
+         xValues.push(x)
+         yValues.push(maksvelsChart(x, M, T))
+      }
+
+		let newChart = {
+			id: id,
+			label: String(id),
+			data: yValues,
+			borderColor: "rgba(247, 34, 34, 0.5)"
+		}
+
+		this.datasetsOptions.push(newChart)
+
+		this.createCharts(xValues)
+	}
+
+	delChart() {
+		//
+	}
+}
+
+function maksvelsChart(v, M, T) {
+   const R = 8.314 // универсальная газовая постоянная в Дж/(моль·К)
+   const coefficient = Math.pow(M / (2 * Math.PI * R * T), 3/2)
+   const exponential = Math.exp(-(M * Math.pow(v, 2)) / (2 * R * T))	
+
+   let result = 4 * Math.PI * coefficient * Math.pow(v, 2) * exponential * 1000
+
+   console.log(M,T,v, result)
+   
+   return result
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,86 +1,17 @@
-let MYCHART = ""
+import ChartSystem from "./Chart.js"
 
-function createChart() {
-	let xValues = []
-	let yValues = []
-
-	function genChart(i1, i2, step = 1) {
-		function maksvelsChart(v, M, T) {
-			const R = 8.314 // универсальная газовая постоянная в Дж/(моль·К)
-
-			const coefficient = Math.pow(M / (2 * Math.PI * R * T), 3/2)
-			const exponential = Math.exp(-(M * Math.pow(v, 2)) / (2 * R * T))	
-
-			let result = 4 * Math.PI * coefficient * Math.pow(v, 2) * exponential *1000
-
-			return result
-		}
-
-		for (let x = i1; x <= i2; x += step) {
-			const M = document.querySelector("#mass").value*10**-3
-			const T = document.querySelector("#temp").value
-
-			yValues.push(maksvelsChart(x, M, T))
-			xValues.push(x)
-		}
-	}
-
-	genChart(0, document.querySelector("#speed").value, 50)
-	
-	if(MYCHART !== ""){
-		MYCHART.destroy()
-	}
-
-	MYCHART = new Chart("myChart", {
-		type: "line",
-		data: {
-			labels: xValues,
-			datasets: [
-				{
-					fill: false,
-					pointRadius: 4,
-					borderColor: "rgba(0, 0, 255, 0.5)",
-					data: yValues
-				}
-			]
-		},
-		options: {
-			plugins: {
-				title: {
-					display: true,
-					text: "Распределение Максвелла-Больцмана"
-				},
-				legend: {
-					display: false
-				}
-			},
-			scales: {
-				x: {
-					title: {
-						display: true,
-						text: "V (м/с)", // Подпись для оси X
-						align: "end",
-						color: "rgb(0, 0, 0)"
-					}
-				},
-				y: {
-					title: {
-						display: true,
-						text: "F(x) (м/c)", // Подпись для оси Y
-						align: "end",
-						color: "rgb(0, 0, 0)"
-					}
-				}
-			}
-		}
-	})
-}
-
+const MyChart = new ChartSystem()
+let id = 0
 document.querySelector("#submit").addEventListener(
 	"submit",
 	(event) => {
 		event.preventDefault()
-		createChart()
+		id++
+		const vmax = document.querySelector("#speed").value
+		const M = document.querySelector("#mass").value*10**-3
+		const T = document.querySelector("#temp").value
+
+		MyChart.addChart(vmax, M, T, id)
 	},
 	false
 )
